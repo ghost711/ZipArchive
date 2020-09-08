a2a57592388177a5a7827430aa88642cdc297195
Jon Mitts <mitts.3@gmail.com>
2020-09-08 08:37:27 -0400

    Added define.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SSZipArchive",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries produced by a package,
+        // and make them visible to other packages.
         .library(name: "SSZipArchive", targets: ["SSZipArchive"]),
     ],
     dependencies: [
@@ -14,9 +15,12 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "SSZipArchive", dependencies: [], path: "SSZipArchive"),
+        // Targets are the basic building blocks of a package. A target can
+        // define a module or a test suite. Targets can depend on other targets
+        // in this package, and on products in packages which this package
+        // depends on.
+        .target(name: "SSZipArchive", dependencies: [], path: "SSZipArchive",
+                                              cSettings: [.define("COCOAPODS")])
         //.testTarget(name: "ZipArchiveTests", dependencies: ["ZipArchive"]),
     ]
 )
